42373a89ccb2e5e01de9f175e1299add8c13b0f2
Michaś (Arlen) <33624810+michalo21@users.noreply.github.com>
2019-04-05 14:30:59 +0200

    Add files via upload

--- Kwadratura.java
+++ Kwadratura.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 public class Kwadratura {
 
      public static double func(double x){ // metoda odpowiedzialna za wprowadzenie funkcji; trzeba samemu wklepać
-        double func = x*x + 3; 
+        double func = 2*x*x*x + 3*x*x; 
         return func;
     }
     public static void main(String[] args) {
@@ -25,18 +25,17 @@ public class Kwadratura {
         int b = sc.nextInt();
         System.out.print("Podaj liczbe przedzaiłow n: ");
         int n = sc.nextInt();
-        double h = (double)((b-a)/n);
+        double h = (b-a)/(double)n;
         double sfpp = 0; // suma wartosci funkcji punktow przedzialowych
         double sfps = 0; // suma wartosci funkcjo w punkcie srodkowym przedzialu
         double x = 0;
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i < n; i++){
             x = a + i*h;  //punkt podzialowy xi
             sfps+=func(x - h/2); // suma wartosci funkcji w punktach srodkowych przedzialow
-            if(i < n){
-                sfpp+=func(x); // suma wartosci wunkcji w punktahc podzialowych xi
-            }
+            sfpp+=func(x); // suma wartosci wunkcji w punktahc podzialowych xi   
         }
-        sfpp = h / 6*(func(a) + func(b) + 4*sfps + 2*sfpp);
+       sfps+=func(b - h/2);
+       sfpp = h / 6*(func(a) + func(b) + 4*sfps + 2*sfpp);
        System.out.print("Wartosc calki dla podanej funkcji to: " + sfpp);
     }
 }
